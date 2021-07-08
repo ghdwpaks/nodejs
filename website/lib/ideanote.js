6ca9fad98009114e96dd84412a1b71bd1d9f24b0
ghdwpaks <hjm7904@gmail.com>
2021-07-09 05:22:31 +0900

    tied up my codes

---
+++ website/lib/ideanote.js
@@ -0,0 +1,78 @@
+var db = require('./db.js');
+var template = require('./template.js');
+var qs = require('querystring');
+
+
+exports.home = function(request,response) {
+    db.query(`SELECT * FROM ideanote`,function(error,ideas){
+        var title = 'welcome';
+        var description = "hello, node.js";
+        var list = template.list(ideas);
+        var html = template.HTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
+        response.writeHead(200);
+        response.end(html);
+    });
+}
+exports.create = function(request,response) {
+    db.query(`SELECT * FROM ideanote`,function(error,ideas){
+        //console.log(topics);
+        var title = 'CREATE';
+        //var description = "hello, node.js";
+        //var list = template.list(topics);
+        var list = "";
+        var html = template.HTML(title,list,`
+
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="적을 내용"></p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+          ``);
+          //`<a href="/create">create</a>`
+        response.writeHead(200);
+        response.end(html);
+      });
+}
+
+exports.create_process = function(request,response) {
+    var body = '';
+    request.on('data', function(data){
+        body = body + data;
+    });
+    request.on('end', function(){
+        var post = qs.parse(body);
+        tempqu = `INSERT INTO ideanote (title, timestamp, creater_id) VALUES (?, NOW(),? );`,[post.title,1]
+        console.log("삽입예정 쿼리문 :",tempqu);
+        db.query(`INSERT INTO ideanote (title, timestamp, creater_id) VALUES (?, NOW(),? );`,[post.title,1],function(error,result){
+          if(error) {
+            throw error;
+          }
+          response.writeHead(302,{location:`/`});
+          response.end();
+        })
+    });
+}
+exports.else = function(request,response) {
+    db.query(`SELECT * FROM ideanote`,function(erorr,ideas){
+        if(erorr) {
+        }
+        db.query(`SELECT * FROM ideanote WHERE id=${queryData.id};`,function(erorr2,idea) {
+          if(erorr2) {
+          }
+          var title = idea[0].title;
+          var description = idea[0].description;
+          var list = template.list(ideas);
+          var html = template.HTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>
+          
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${queryData.id}">
+            
+          </form>`);
+          response.writeHead(200);
+          response.end(html);
+        })
+      }) 
+}
+
--- website/main.js
+++ website/main.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
-
-var db = require('./lib/db');
+var ideanote = require("./lib/ideanote.js");
+var db = require('./lib/db.js');
 
 
 
@@ -17,82 +17,16 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/'){
       if(queryData.id === undefined) {
-        db.query(`SELECT * FROM ideanote`,function(error,ideas){
-          var title = 'welcome';
-          var description = "hello, node.js";
-          var list = template.list(ideas);
-          var html = template.HTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
-          response.writeHead(200);
-          response.end(html);
-
-        });
+        ideanote.home(request,response); 
       } else {
-        //console.log("else 진입1")
-        db.query(`SELECT * FROM ideanote`,function(erorr,ideas){
-          if(erorr) {
-            //console.log(erorr)
-          }
-          //console.log(queryData.id)
-          db.query(`SELECT * FROM ideanote WHERE id=${queryData.id};`,function(erorr2,idea) {
-            if(erorr2) {
-              //console.log(erorr2)
-            }
-            //console.log(idea[0].title);
-            var title = idea[0].title;
-            var description = idea[0].description;
-            var list = template.list(ideas);
-            var html = template.HTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>
-            
-            <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${queryData.id}">
-              
-            </form>`);
-            //<input type="submit" value="delete">
-            //<a href="/update?id=${queryData.id}">update</a>
-            response.writeHead(200);
-            response.end(html);
-          })
-        }) 
-      }
+        ideanote.else(Request,response);}
     } else if (pathname === '/create') {
-      db.query(`SELECT * FROM ideanote`,function(error,ideas){
-        //console.log(topics);
-        var title = 'CREATE';
-        //var description = "hello, node.js";
-        //var list = template.list(topics);
-        var list = "";
-        var html = template.HTML(title,list,`
-
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="적을 내용"></p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-          `,
-          ``);
-          //`<a href="/create">create</a>`
-        response.writeHead(200);
-        response.end(html);
-      });
+      ideanote.create(request,response);
     }else if(pathname === '/create_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          tempqu = `INSERT INTO ideanote (title, timestamp, creater_id) VALUES (?, NOW(),? );`,[post.title,1]
-          console.log("삽입예정 쿼리문 :",tempqu);
-          db.query(`INSERT INTO ideanote (title, timestamp, creater_id) VALUES (?, NOW(),? );`,[post.title,1],function(error,result){
-            if(error) {
-              throw error;
-            }
-            response.writeHead(302,{location:`/?id=${result.insertId}`});
-            response.end();
-          })
-      });
-    } else if(pathname === '/delete'){
+      ideanote.create_process(request,response);
+
+
+    } else if(pathname === '/deleteprocess'){
       console.log("delete 진입 및 queryData값 :",queryData)
       request.on('data',function(data){
         body = body + data;
